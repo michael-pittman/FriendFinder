4b63ab07e67f4a6730d54681814aff9ed772c3ed
michaelc.pittman <michaelc.pittman@gmail.com>
2017-08-17 18:16:18 -0400

    Initial push

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes");
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
